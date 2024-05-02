461a34c7593cb37c1292ac69cd6af2e7190cfbe2
Abdul Wahab Malik <abdul.wahab.malik@deutics.com>
2024-05-02 15:58:49 +0500

    zone intrusion done

--- Features/ZoneIntrusionDetector/ZoneIntrusionDetector.py
+++ Features/ZoneIntrusionDetector/ZoneIntrusionDetector.py
@@ -1,10 +1,15 @@
 # Tracker
+import json
+import mmap
+
+import time
+import socket
+
 import cv2
+import numpy as np
 
 from Utils.Tracker.DeepSort.ObjectTracker import ObjectTracker
 
-# Zones
-from Utils.Zone.Zone import Zone
 from Utils.Zone.TimeSpecificZone import TimeSpecificZone
 from Utils.MulticastSocket.MulticastSocket import MulticastSocket
 
@@ -18,84 +23,94 @@ from Utils.structures.camera_info import CameraInfo
 from Features.utils import *
 from datetime import datetime
 
-import threading
-
 
 class ZoneIntrusionDetector:
-    def __init__(self, region_coordinates,
-                 instance_id,
-                 camera_location,
-                 camera_name,
-                 expected_objs=None,
-                 object_size=None,
+    def __init__(self, server_ip,
+                 server_port,
                  zone_dwelling_time_in_seconds=None,
                  time_bounds=None,
                  ):
+        try:
+            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.server_socket.bind((server_ip, server_port))
+
+        except (socket.error, ConnectionRefusedError, TimeoutError) as err:
+            print("Socket connection error:", err)
 
         if time_bounds is None:
             time_bounds = [{"starting_time": "0:0", "ending_time": "23:59"}]
 
         self._time_bounds = time_bounds
 
-        self._obj_size = object_size
-        self._instance_id = instance_id
-        self._expected_objs = expected_objs
+        self.multicast_socket = MulticastSocket(multicast_address="234.100.0.1",
+                                                multicast_port=8088)
+
+        self._expected_objs = ""
         self._zone_dwelling_time_in_seconds = zone_dwelling_time_in_seconds
 
-        self._polygon_zone = TimeSpecificZone(zone_coordinates=region_coordinates,
-                                              zone_dwelling_time_in_seconds=self._zone_dwelling_time_in_seconds)
+        self._polygon_zone = None
 
-        self._object_tracker = ObjectTracker(use_gpu=True,
-                                             obj_size=self._obj_size,
-                                             expected_objs=expected_objs,
-                                             detector="Yolov8",
-                                             detector_model="yolov8s.pt")
+        self._object_tracker = None
 
-        self.multicast_socket = MulticastSocket(multicast_address="224.1.1.1",
-                                                multicast_port=1234)
         self.region_min_x, self.region_min_y, self.region_max_x, self.region_max_y = 0, 0, 0, 0
         self._first_frame = 1
-        self.camera_location = camera_location
-        self.camera_name = camera_name
+        self.camera_id = 0
         self.video_width = 0
         self.video_height = 0
+        self.algo_type = 0
+        self.camera_polygons = None
+        self.total_camera_polygons = -1
+        self.data_size = 0
+        self.frame_number = 0
+        self.last_detection_time = 0
+        self.detection_cam_config = None
 
-    def process_video(self, streaming_source):
+    def process_video(self):
         """******************************
         Functionality: read the frame of video, and send it to function process image
         Parameters: None
         Returns: None
         *********************************"""
 
-        cap = cv2.VideoCapture(streaming_source)
-
         while True:
-            is_frame, frame = cap.read()
+            frame = self.get_frame_from_socket()
 
-            if not is_frame:
-                break
+            if frame is None:
+                continue
 
+            # start_time = time.time()
             self.process_frame(frame)
+            # end_time = time.time()
+            # print((end_time-start_time)*1000)
+            # cv2.imwrite("temp.jpg", frame)
 
             cv2.imshow("Zone Intrusion", frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        cap.release()
+            cv2.waitKey(1)
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #    break
+        # cap.release()
         cv2.destroyAllWindows()
 
     def process_frame(self, frame):
+
         if self._first_frame:
-            self.video_height, self.video_width, _ = frame.shape
-            # self._polygon_zone.create_polygon_mask(img_size=(self.video_height, self.video_width))
-            points = self._polygon_zone.zone_coordinates
+            self.total_camera_polygons = len(self.camera_polygons)
+            # if self.total_camera_polygons == 1:
+            points = self._polygon_zone.zone_coordinates[0]
             self.region_min_x, self.region_min_y = max(0, points.min(axis=0)[0]), max(0, points.min(axis=0)[1])
             self.region_max_x, self.region_max_y = min(frame.shape[1], points.max(axis=0)[0]), points.max(axis=0)[1]
+            self._polygon_zone.create_polygon_mask((self.video_height, self.video_width))
             self._first_frame = 0
 
-        extracted_rectangle = frame[self.region_min_y:self.region_max_y,
-                                    self.region_min_x:self.region_max_x]
+        region_of_interest = cv2.bitwise_and(frame, frame, mask=self._polygon_zone.polygon_binary_mask)
 
-        tracked_objects = self._object_tracker.process_frame(extracted_rectangle)
+        if self.total_camera_polygons == 1:
+            region_of_interest = region_of_interest[self.region_min_y:self.region_max_y,
+                                                    self.region_min_x:self.region_max_x]
+        # else:
+        #     region_of_interest = cv2.bitwise_and(frame, frame, mask=self._polygon_zone.polygon_binary_mask)
+
+        tracked_objects = self._object_tracker.process_frame(region_of_interest)
 
         frame = self._draw_polygon(frame)
 
@@ -104,44 +119,22 @@ class ZoneIntrusionDetector:
 
         return tracked_objects
 
+    # def no_detection_thread(self):
+    #     last_detection = -1     # so it would activate for the first frame
+    #     start_time = time.time()
+    #     while True:
+    #         print("thread_time ", time.time() - start_time)
+    #         if last_detection == self.detection_counter:
+    #             last_detection = self.detection_counter
+    #             print("zero detection in last second")
+    #             # generate coe
+    #         time.sleep(1)   # sleep for 1 second
+
     def _draw_polygon(self, frame):
-
-        pts = self._polygon_zone.zone_coordinates.reshape((-1, 1, 2))
-        frame = cv2.polylines(frame, [pts], isClosed=True, color=(0, 0, 255), thickness=2)
+        for polygon in self._polygon_zone.zone_coordinates:
+            pts = polygon.reshape((-1, 1, 2))
+            frame = cv2.polylines(frame, [pts], isClosed=True, color=(0, 0, 255), thickness=2)
         return frame
-
-    # def _check_zone_intrusion(self, frame, detections):
-    #     """*********************************
-    #     Functionality: Check if any of detected objects has intruded the defined zone
-    #     Parameter: None
-    #     Return: None
-    #     ************************************"""
-    #     tracks = self._object_tracker.tracker.get_tracks()
-    #     intruded_objects = {}
-    #
-    #     for i, track in enumerate(tracks):
-    #         if not track.is_missed:
-    #             if self._polygon_zone.if_zone_intruded(track):
-    #                 bbox = track.get_state()[0]
-    #                 track_id = track.track_id
-    #                 label = self._object_tracker.classes[track.class_id.item()]
-    #
-    #                 txt_tobe_print = f'{track_id}' + " " + f'{label}'
-    #                 plot_one_box(bbox, frame, label=txt_tobe_print, color=(0, 0, 255), line_thickness=1)
-    #
-    #                 intruded_object = ALGO_DETECTION_OBJECT_DATA(X=int(bbox[0]), Y=int(bbox[1]),
-    #                                                              Width=int(bbox[2]-bbox[0]), Height=int(bbox[3]-bbox[1]),
-    #                                                              CountUpTime=0, ObjectType=label, frameNum=0,
-    #                                                              ID=track_id, polygonID=0)
-    #
-    #                 if label in intruded_objects:
-    #                     intruded_objects[label].append(intruded_object)
-    #                 else:
-    #                     intruded_objects[label] = [intruded_object]
-    #
-    #     if len(intruded_objects):
-    #         thread = threading.Thread(target=self.create_structure, args=(intruded_objects,))
-    #         thread.start()
 
     def _check_zone_intrusion(self, frame, detections):
         """*********************************
@@ -153,60 +146,166 @@ class ZoneIntrusionDetector:
         intruded_objects = {}
 
         for i, track in enumerate(tracks):
-            if not track.is_missed:
-                bbox = track.get_state()[0]
-                if not len(bbox):
-                    continue
+            if track.is_missed or not len(track.get_state()[0]):
+                continue
 
-                xmin, ymin, xmax, ymax = bbox
-                track_id = track.track_id
-                label = self._object_tracker.classes[track.class_id.item()]
-                # points = self._polygon_zone.zone_coordinates
-                # min_x, min_y = max(0, points.min(axis=0)[0]), max(0, points.min(axis=0)[1])
-                bbox = [xmin + self.region_min_x, ymin + self.region_min_y,
-                        xmax + self.region_min_x, ymax + self.region_min_y]
+            bbox = track.get_state()[0]
+            # if not len(bbox):
+            #     continue
 
-                txt_tobe_print = f'{track_id}' + " " + f'{label}'
-                plot_one_box(bbox, frame, label=txt_tobe_print, color=(0, 0, 255), line_thickness=1)
-                intruded_object = ALGO_DETECTION_OBJECT_DATA(X=int(bbox[0]), Y=int(bbox[1]),
-                                                             Width=int(bbox[2] - bbox[0]),
-                                                             Height=int(bbox[3] - bbox[1]),
-                                                             CountUpTime=0, ObjectType=label, frameNum=0,
-                                                             ID=track_id, polygonID=0)
+            xmin, ymin, xmax, ymax = bbox
+            track_id = track.track_id
 
-                if label in intruded_objects:
-                    intruded_objects[label].append(intruded_object)
-                else:
-                    intruded_objects[label] = [intruded_object]
+            label = self._object_tracker.classes[track.class_id.item()]
+
+            bbox = [xmin + self.region_min_x, ymin + self.region_min_y,
+                    xmax + self.region_min_x, ymax + self.region_min_y]
+
+            txt_tobe_print = f'{track_id}' + " " + f'{label}'
+            plot_one_box(bbox, frame, label=txt_tobe_print, color=(0, 0, 255), line_thickness=1)
+
+            intruded_object = ALGO_DETECTION_OBJECT_DATA(X=int(bbox[0]), Y=int(bbox[1]),
+                                                         Width=int(bbox[2] - bbox[0]),
+                                                         Height=int(bbox[3] - bbox[1]),
+                                                         CountUpTime=0, ObjectType=label, frameNum=self.frame_number,
+                                                         ID=track_id, polygonID=self.camera_polygons[0].PolygonId,
+                                                         DetectionPercentage=100)
+
+            if label in intruded_objects:
+                intruded_objects[label].append(intruded_object)
+            else:
+                intruded_objects[label] = [intruded_object]
 
         if len(intruded_objects):
-            thread = threading.Thread(target=self.create_structure, args=(intruded_objects,))
-            thread.start()
+            self._last_detection_time = time.time()
+            self.create_structure(intruded_objects)
+        else:
+            if time.time() - self._last_detection_time >= 1:
+                self.send_clearance_message()
 
     def create_structure(self, intruded_objects_data):
         intruded_objects_by_type = []
+
         for key, value in intruded_objects_data.items():
             intruded_object_by_type = ALGO_DETECTION_OBJECTS_BY_TYPE(objectCount=len(value),
-                                                                     alarmSet=False,
+                                                                     alarmSet=True,
                                                                      objectsType=key,
                                                                      algObject=value)
+
             intruded_objects_by_type.append(intruded_object_by_type)
 
-        camera_polygon = [CameraPolygon(camID=1, polygonId=1, detectionType="car", maxAllowed=10, polygon=[1, 2, 3])]
-        camera_info = CameraInfo(polygon_available=True, video_counter=1, video_width=1920, video_height=1080)
-
-        detection_cam_config = DetectionCameraConfig(cameraPolygon=camera_polygon, cameraInfo=camera_info)
-
-        intruded_objects = ALGO_DETECTION_OBJECT(cameraLocation=self.camera_location,
-                                                 cameraName=self.camera_name,
+        intruded_objects = ALGO_DETECTION_OBJECT(cameraId=self.camera_id,
                                                  totalObjectCount=len(intruded_objects_by_type),
                                                  videoWidth=self.video_width,
                                                  videoHeight=self.video_height,
                                                  dateTime=datetime.now(),
                                                  AlgoType=1,
                                                  videoCounter=1,
-                                                 detectionCameraConfig=detection_cam_config,
+                                                 detectionCameraConfig=self.detection_cam_config,
                                                  algoObject=intruded_objects_by_type
                                                  )
 
         self.multicast_socket.send_detection(intruded_objects.toJSON().encode('utf-8'))
+
+    def get_frame_from_socket(self):
+        data, client_address = self.server_socket.recvfrom(1024)
+        print(data)
+
+        data_dict = data.decode("utf-8")
+
+        try:
+            data_dict = json.loads(data_dict)
+        except json.JSONDecodeError:
+            print("Error: Invalid JSON format in data_type")
+
+        frame = None
+        if int(data_dict["Opcode"]) == 10:
+            print(data_dict)
+            # print("workinh")
+            self.camera_id = data_dict['cameraId']
+            self.algo_type = data_dict['algoType']
+            self.video_width = data_dict['videoWidth']
+            self.video_height = data_dict['videoHeight']
+            self.data_size = data_dict['dataSize']
+
+            try:
+                self.camera_polygons = [
+                    CameraPolygon(**polygon_data) for polygon_data in data_dict["CameraPolygon"]
+                ]
+            except (TypeError, KeyError) as e:
+                print(f"Error mapping polygons: {e}")
+
+            self._expected_objs = self.camera_polygons[0].DetectionType.split(",")
+            self._expected_objs = [expected_object.strip() for expected_object in self._expected_objs]
+
+            camera_info = CameraInfo(polygon_available=True,
+                                     video_counter=1,
+                                     video_width=self.video_width,
+                                     video_height=self.video_height)
+
+            self.detection_cam_config = DetectionCameraConfig(cameraPolygon=self.camera_polygons, cameraInfo=camera_info)
+            self.initialize_ai_instance()
+
+        elif int(data_dict["Opcode"]) == 1:
+            print("opcode1")
+            buffer_index = data_dict["bufferIndex"]
+            self.frame_number = data_dict["frameNumber"]
+
+            frame = self.process_buffer(buffer_index)
+
+        return frame
+
+    def initialize_ai_instance(self):
+
+        converted_coordinates = self.get_all_polygons(self.camera_polygons)
+        self._polygon_zone = TimeSpecificZone(zone_coordinates=converted_coordinates,
+                                              zone_dwelling_time_in_seconds=self._zone_dwelling_time_in_seconds)
+
+        self._object_tracker = ObjectTracker(use_gpu=True,
+                                             obj_size=None,
+                                             expected_objs=self._expected_objs,
+                                             detector="Yolov8",
+                                             detector_model="yolov8s.pt")
+
+    def process_buffer(self, buffer_index):
+
+        mm_file_name = f"ShovalSCMMap{buffer_index}_vid_{0}"
+        #print(mm_file_name)
+        with (mmap.mmap(-1, self.video_width * self.video_height * 3, access=mmap.ACCESS_READ, tagname=mm_file_name)
+              as mm):
+
+            buf = mm.read()
+
+        # width = 1920
+        # height = 1080
+        # num_channels = 3  # RGB image
+
+        image_array = np.frombuffer(buf, dtype=np.uint8).reshape(self.video_height, self.video_width, 3)
+
+        return image_array
+
+    def send_clearance_message(self):
+        # dummy_object = [ALGO_DETECTION_OBJECT_DATA(X=0, Y=0, Width=0, Height=0, CountUpTime=0, ObjectType="clear",
+        #                                           frameNum=self.frame_number, ID=-1,
+        #                                           polygonID=self.camera_polygons[0].PolygonId, DetectionPercentage=100)]
+
+        self.create_structure({"dummy": []})
+
+
+    @staticmethod
+    def get_all_polygons(camera_polygons):
+
+        all_polygons = []
+
+        for polygon_data in camera_polygons:
+            polygon = polygon_data.Polygon
+
+            polygon_tuples = [(polygon[i], polygon[i + 1]) for i in
+                              range(0, len(polygon), 2)]  # Convert to coordinate tuples
+
+            all_polygons.append(polygon_tuples)
+        return all_polygons
+
+
+
+
--- main.py
+++ main.py
@@ -5,14 +5,9 @@ from Features.ZoneIntrusionDetector.ZoneIntrusionDetector import ZoneIntrusionDe
 # main function
 if __name__ == '__main__':
 
-    zi = ZoneIntrusionDetector(region_coordinates=[[20, 0], [800, 0], [800, 900], [20, 900]],
-                               instance_id=1,
-                               expected_objs=["person", "car", "bus", "truck", "motorcycle",
-                                              "bicycle", "train", "boat", "suitcase"],
-                               zone_dwelling_time_in_seconds=0,
-                               camera_name="road",
-                               camera_location="office")
+    zi = ZoneIntrusionDetector(zone_dwelling_time_in_seconds=0,
+                               server_ip="127.0.0.1",
+                               server_port=7111)
 
-    zi.process_video("udp://@234.7.7.7:7777?overrun_nonfatal=1&fifo_size=50000000")
-    # zi.process_video("udp://@224.1.1.1/1234")
-
+    zi.process_video()
+    # zi.process_video("udp://@234.7.7.7:7777?")
